dcb681db2b0fa44266c7858975ce520d99837efb
Daryl Song <songlwdaryl@gmail.com>
2022-08-31 15:27:35 +0800

    completed up to 06

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,151 +3,188 @@
 
 import * as React from 'react'
 import {
-  useForceRerender,
-  useDebouncedState,
-  AppGrid,
-  updateGridState,
-  updateGridCellState,
+    useForceRerender,
+    useDebouncedState,
+    AppGrid,
+    updateGridState,
+    updateGridCellState,
 } from '../utils'
 
 const AppStateContext = React.createContext()
 const AppDispatchContext = React.createContext()
+const DogNameContext = React.createContext()
 
-const initialGrid = Array.from({length: 100}, () =>
-  Array.from({length: 100}, () => Math.random() * 100),
+const initialGrid = Array.from({ length: 100 }, () =>
+    Array.from({ length: 100 }, () => Math.random() * 100),
 )
 
 function appReducer(state, action) {
-  switch (action.type) {
-    // we're no longer managing the dogName state in our reducer
-    // 💣 remove this case
-    case 'TYPED_IN_DOG_INPUT': {
-      return {...state, dogName: action.dogName}
+    switch (action.type) {
+        case 'UPDATE_GRID_CELL': {
+            return { ...state, grid: updateGridCellState(state.grid, action) }
+        }
+        case 'UPDATE_GRID': {
+            return { ...state, grid: updateGridState(state.grid) }
+        }
+        default: {
+            throw new Error(`Unhandled action type: ${action.type}`)
+        }
     }
-    case 'UPDATE_GRID_CELL': {
-      return {...state, grid: updateGridCellState(state.grid, action)}
-    }
-    case 'UPDATE_GRID': {
-      return {...state, grid: updateGridState(state.grid)}
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
-    }
-  }
 }
 
-function AppProvider({children}) {
-  const [state, dispatch] = React.useReducer(appReducer, {
-    // 💣 remove the dogName state because we're no longer managing that
-    dogName: '',
-    grid: initialGrid,
-  })
-  return (
-    <AppStateContext.Provider value={state}>
-      <AppDispatchContext.Provider value={dispatch}>
-        {children}
-      </AppDispatchContext.Provider>
-    </AppStateContext.Provider>
-  )
+function AppProvider({ children }) {
+    const [state, dispatch] = React.useReducer(appReducer, {
+        grid: initialGrid,
+    })
+    return (
+        <AppStateContext.Provider value={state}>
+            <AppDispatchContext.Provider value={dispatch}>
+                {children}
+            </AppDispatchContext.Provider>
+        </AppStateContext.Provider>
+    )
 }
 
 function useAppState() {
-  const context = React.useContext(AppStateContext)
-  if (!context) {
-    throw new Error('useAppState must be used within the AppProvider')
-  }
-  return context
+    const context = React.useContext(AppStateContext)
+    if (!context) {
+        throw new Error('useAppState must be used within the AppProvider')
+    }
+    return context
 }
 
 function useAppDispatch() {
-  const context = React.useContext(AppDispatchContext)
-  if (!context) {
-    throw new Error('useAppDispatch must be used within the AppProvider')
-  }
-  return context
+    const context = React.useContext(AppDispatchContext)
+    if (!context) {
+        throw new Error('useAppDispatch must be used within the AppProvider')
+    }
+    return context
+}
+
+function dogNameReducer(state, action) {
+    switch (action.type) {
+        case 'UPDATE_DOG_NAME': {
+            return { ...state, dogName: action.dogName }
+        }
+        default: {
+            throw new Error(`Unhandled action type: ${action.type}`)
+        }
+    }
+}
+
+function DogNameProvider({ children }) {
+    const [state, dispatch] = React.useReducer(dogNameReducer, {
+        dogName: '',
+    })
+    const value = [state, dispatch]
+    return (
+        <DogNameContext.Provider value={value}>
+            {children}
+        </DogNameContext.Provider>
+    )
+}
+
+function useDogName() {
+    const context = React.useContext(DogNameContext)
+    if (!context) {
+        throw new Error('useDogName must be used within the DogNameProvider')
+    }
+    return context
 }
 
 function Grid() {
-  const dispatch = useAppDispatch()
-  const [rows, setRows] = useDebouncedState(50)
-  const [columns, setColumns] = useDebouncedState(50)
-  const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
-  return (
-    <AppGrid
-      onUpdateGrid={updateGridData}
-      rows={rows}
-      handleRowsChange={setRows}
-      columns={columns}
-      handleColumnsChange={setColumns}
-      Cell={Cell}
-    />
-  )
+    const dispatch = useAppDispatch()
+    const [rows, setRows] = useDebouncedState(50)
+    const [columns, setColumns] = useDebouncedState(50)
+    const updateGridData = () => dispatch({ type: 'UPDATE_GRID' })
+    return (
+        <AppGrid
+            onUpdateGrid={updateGridData}
+            rows={rows}
+            handleRowsChange={setRows}
+            columns={columns}
+            handleColumnsChange={setColumns}
+            Cell={Cell}
+        />
+    )
 }
 Grid = React.memo(Grid)
 
-function Cell({row, column}) {
-  const state = useAppState()
-  const cell = state.grid[row][column]
-  const dispatch = useAppDispatch()
-  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
-  return (
-    <button
-      className="cell"
-      onClick={handleClick}
-      style={{
-        color: cell > 50 ? 'white' : 'black',
-        backgroundColor: `rgba(0, 0, 0, ${cell / 100})`,
-      }}
-    >
-      {Math.floor(cell)}
-    </button>
-  )
+function withStateSlice(Comp, slice) {
+    const MemoComp = React.memo(Comp)
+    function Wrapper(props) {
+        const state = useAppState()
+        return (
+            <MemoComp state={slice(state, props)} {...props} />
+        )
+    }
+    Wrapper.displayName = `withStateSlice(${Comp.displayName || Comp.name})`
+    Wrapper = React.memo(Wrapper)
+    return Wrapper
 }
-Cell = React.memo(Cell)
+
+function Cell({ state: cell, row, column }) {
+    const dispatch = useAppDispatch()
+    const handleClick = () => dispatch({ type: 'UPDATE_GRID_CELL', row, column })
+    return (
+        <button
+            className="cell"
+            onClick={handleClick}
+            style={{
+                color: cell > 50 ? 'white' : 'black',
+                backgroundColor: `rgba(0, 0, 0, ${cell / 100})`,
+            }}
+        >
+            {Math.floor(cell)}
+        </button>
+    )
+}
+Cell = withStateSlice(Cell, (state, {row, column}) => state.grid[row][column])
 
 function DogNameInput() {
-  // 🐨 replace the useAppState and useAppDispatch with a normal useState here
-  // to manage the dogName locally within this component
-  const state = useAppState()
-  const dispatch = useAppDispatch()
-  const {dogName} = state
+    const [state, dispatch] = useDogName()
+    const dogName = state.dogName
+    const updateDogName = (newDogName) => dispatch({ type: 'UPDATE_DOG_NAME', dogName: newDogName })
 
-  function handleChange(event) {
-    const newDogName = event.target.value
-    // 🐨 change this to call your state setter that you get from useState
-    dispatch({type: 'TYPED_IN_DOG_INPUT', dogName: newDogName})
-  }
+    function handleChange(event) {
+        const newDogName = event.target.value
+        updateDogName(newDogName)
+    }
 
-  return (
-    <form onSubmit={e => e.preventDefault()}>
-      <label htmlFor="dogName">Dog Name</label>
-      <input
-        value={dogName}
-        onChange={handleChange}
-        id="dogName"
-        placeholder="Toto"
-      />
-      {dogName ? (
-        <div>
-          <strong>{dogName}</strong>, I've a feeling we're not in Kansas anymore
-        </div>
-      ) : null}
-    </form>
-  )
+    return (
+
+        <form onSubmit={e => e.preventDefault()}>
+            <label htmlFor="dogName">Dog Name</label>
+            <input
+                value={dogName}
+                onChange={handleChange}
+                id="dogName"
+                placeholder="Toto"
+            />
+            {dogName ? (
+                <div>
+                    <strong>{dogName}</strong>, I've a feeling we're not in Kansas anymore
+                </div>
+            ) : null}
+        </form>
+
+    )
 }
 function App() {
-  const forceRerender = useForceRerender()
-  return (
-    <div className="grid-app">
-      <button onClick={forceRerender}>force rerender</button>
-      <AppProvider>
-        <div>
-          <DogNameInput />
-          <Grid />
+    const forceRerender = useForceRerender()
+    return (
+        <div className="grid-app">
+            <button onClick={forceRerender}>force rerender</button>
+            <div>
+                <DogNameProvider>
+                    <DogNameInput />
+                </DogNameProvider>
+                <AppProvider>
+                    <Grid />
+                </AppProvider>
+            </div>
         </div>
-      </AppProvider>
-    </div>
-  )
+    )
 }
 
 export default App
